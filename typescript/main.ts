bb74ed53ac7cf47e01f642826f7b6dc699f66a2f
Boshen <boshenc@gmail.com>
2020-12-04 02:22:25 +0800

    refactor(typescript): reduce boilerplace by converting class to interface

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,4 +1,4 @@
-class Vector {
+class V3 {
   constructor(
     public x: number,
     public y: number,
@@ -9,52 +9,32 @@ class Vector {
     return Math.sqrt(this.dot(this))
   }
 
-  public dot(v: Vector) {
+  public dot(v: V3) {
     return this.x * v.x + this.y * v.y + this.z * v.z
   }
 
-  public add(v: Vector): Vector {
-    return new Vector(
-      this.x + v.x,
-      this.y + v.y,
-      this.z + v.z
-    )
+  public add(v: V3): V3 {
+    return new V3(this.x + v.x, this.y + v.y, this.z + v.z)
   }
 
-  public translate(n: number): Vector {
-    return new Vector(
-      this.x + n,
-      this.y + n,
-      this.z + n
-    )
+  public translate(n: number): V3 {
+    return new V3(this.x + n, this.y + n, this.z + n)
   }
 
-  public sub(v: Vector): Vector {
-    return new Vector(
-      this.x - v.x,
-      this.y - v.y,
-      this.z - v.z
-    )
+  public sub(v: V3): V3 {
+    return new V3(this.x - v.x, this.y - v.y, this.z - v.z)
   }
 
-  public scale(p: number): Vector {
-    return new Vector(
-      p * this.x,
-      p * this.y,
-      p * this.z
-    )
+  public scale(p: number): V3 {
+    return new V3(p * this.x, p * this.y, p * this.z)
   }
 
-  public mul(p: Vector): Vector {
-    return new Vector(
-      p.x * this.x,
-      p.y * this.y,
-      p.z * this.z
-    )
+  public mul(p: V3): V3 {
+    return new V3(p.x * this.x, p.y * this.y, p.z * this.z)
   }
 
-  public cross(v: Vector): Vector {
-    return new Vector(
+  public cross(v: V3): V3 {
+    return new V3(
       (this.y * v.z) - (this.z * v.y),
       (this.z * v.x) - (this.x * v.z),
       (this.x * v.y) - (this.y * v.x)
@@ -72,11 +52,11 @@ class Vector {
 
 class Line {
   constructor(
-    public origin: Vector,
-    public line: Vector
+    public origin: V3,
+    public line: V3
   ) { }
 
-  public getPoint(distance: number): Vector {
+  public getPoint(distance: number): V3 {
     return this.origin.add(this.line.scale(distance))
   }
 
@@ -85,46 +65,36 @@ class Line {
   }
 }
 
-class Light {
-  constructor(
-    public radiance: number,
-    public lightColor: Vector,
-  ) { }
+interface AmbientLight {
+  type: 'ambient'
+  radiance: number
+  color: V3
 }
 
-class AmbientLight extends Light {
+interface DirectionalLight {
+  type: 'directional'
+  radiance: number
+  color: V3
+  location: V3
 }
 
-class DirectionalLight extends Light {
-  constructor(
-    public radiance: number,
-    public lightColor: Vector,
-    public lightLocation: Vector,
-  ) {
-    super(radiance, lightColor)
-  }
+interface PointLight {
+  type: 'point'
+  radiance: number
+  color: V3
+  location: V3
 }
 
-class PointLight extends Light {
-  constructor(
-    public radiance: number,
-    public lightColor: Vector,
-    public lightLocation: Vector,
-  ) {
-    super(radiance, lightColor)
-  }
+type Light = AmbientLight | DirectionalLight | PointLight
+
+interface RayHit {
+  ray: Line
+  point: V3
+  normal: V3
+  distance: number
 }
 
-class RayHit {
-  constructor(
-    public hitRay: Line,
-    public hitPoint: Vector,
-    public hitNormmal: Vector,
-    public hitDistance: number
-  ) { }
-}
-
-const solveq = (a, b, c) => {
+const solveq = (a: number, b: number, c: number): number[] => {
   const d = b * b - 4 * a * c
   if (d < 0) {
     return []
@@ -138,15 +108,15 @@ const solveq = (a, b, c) => {
 class Sphere {
   constructor(
     public radius: number,
-    public center: Vector,
+    public center: V3,
     public diffuseReflection: number ,
-    public diffuseColor: Vector,
+    public diffuseColor: V3,
     public reflection: number,
     public specularRefection: number,
     public shininess: number
   ) { }
 
-  public normal(p: Vector): Vector {
+  public normal(p: V3): V3 {
     return p.sub(this.center)
   }
 
@@ -157,10 +127,15 @@ class Sphere {
     if (roots.length === 0) {
       return null
     } else {
-      const hitDistance = Math.min(...roots)
-      const hitPoint = ray.origin.add(ray.line.scale(hitDistance))
-      const hitNormal = hitPoint.sub(this.center).unit()
-      return new RayHit(ray, hitPoint, hitNormal, hitDistance)
+      const distance = Math.min(...roots)
+      const point = ray.origin.add(ray.line.scale(distance))
+      const normal = point.sub(this.center).unit()
+      return {
+        ray,
+        point,
+        normal,
+        distance
+      } as RayHit
     }
   }
 }
@@ -176,7 +151,7 @@ class Canvas {
     document.body.appendChild(this.canvas)
   }
 
-  public addPixel(i: number, j: number, color: Vector) {
+  public addPixel(i: number, j: number, color: V3) {
     const r = Math.round(color.x)
     const g = Math.round(color.y)
     const b = Math.round(color.z)
@@ -199,20 +174,20 @@ const main = () => {
   const width = 500
   const height = 500
 
-  const lookat = new Vector(0, 0, -50)
-  const eye = new Vector(0, -100, 500)
+  const lookat = new V3(0, 0, -50)
+  const eye = new V3(0, -100, 500)
   const ww = eye.sub(lookat).unit()
-  const vv = new Vector(0, 1, 0)
+  const vv = new V3(0, 1, 0)
   const uu = vv.cross(ww).unit()
   const viewDistance = 400
 
-  const lights = [
-    new AmbientLight(0.1, new Vector(0.05, 0.05, 0.05)),
-    new DirectionalLight(1, new Vector(1, 1, 1), new Vector(1, -1, 0)),
-    new PointLight(3, new Vector(1, 1, 1), new Vector(1000, -5000, 0)),
+  const lights: Light[] = [
+    {type:'ambient', radiance: 0.1, color: new V3(0.05, 0.05, 0.05)},
+    {type:'directional', radiance: 1, color: new V3(1, 1, 01), location: new V3(1, -1, 0)},
+    {type:'point', radiance: 3, color: new V3(1, 1, 1), location: new V3(1000, -5000, 0)},
   ]
 
-  const background = new Vector(0, 0, 0)
+  const background = new V3(0, 0, 0)
 
   const spheres: Sphere[] = []
   for (let i = -1; i < 2; i++) {
@@ -220,9 +195,9 @@ const main = () => {
       spheres.push(
         new Sphere(
           50,
-          new Vector(150 * i, 50, 200 * j),
+          new V3(150 * i, 50, 200 * j),
           0.8,
-          new Vector(Math.max(0, i), Math.max(0, j), Math.max(0, i * j)),
+          new V3(Math.max(0, i), Math.max(0, j), Math.max(0, i * j)),
           0.2,
           0.2,
           20
@@ -231,9 +206,9 @@ const main = () => {
     }
   }
 
-  function trace(ray: Line, depth: number, object?: Sphere): Vector | null {
+  function trace(ray: Line, depth: number, object?: Sphere): V3 | null {
     if (depth > 3) {
-      return new Vector(0, 0, 0)
+      return new V3(0, 0, 0)
     }
 
     const hits = spheres.map((object) => {
@@ -248,56 +223,56 @@ const main = () => {
 
     let hit = hits[0]
     hits.slice(1).forEach((h) => {
-      if (h.hitRay.hitDistance < hit.hitRay.hitDistance) {
+      if (h.hitRay.distance < hit.hitRay.distance) {
         hit = h
       }
     })
 
     return lights
       .map((l) => calcShade(hit.object, hit.hitRay, l))
-      .reduce((a, b) => a.add(b), new Vector(0, 0, 0))
+      .reduce((a, b) => a.add(b), new V3(0, 0, 0))
   }
 
-  function calcShade(object: Sphere, hitRay: RayHit, light: Light): Vector {
+  function calcShade(object: Sphere, hitRay: RayHit, light: Light): V3 {
     const kd = object.diffuseReflection
     const cd = object.diffuseColor
-    const n = hitRay.hitNormmal
-    const cl = light.lightColor
+    const n = hitRay.normal
+    const cl = light.color
     const ls = light.radiance
-    const s = hitRay.hitRay.origin
-    const p = hitRay.hitPoint
+    const s = hitRay.ray.origin
+    const p = hitRay.point
 
-    if (light instanceof AmbientLight) {
-      return cd.scale(kd).mul(light.lightColor.scale(light.radiance))
-    }
-
-    if (light instanceof DirectionalLight) {
-      const l = light.lightLocation.sub(p).unit()
-      return cd.scale(kd).scale(1 / 3.14).scale(Math.max(0, n.dot(l))).mul(cl.scale(ls))
-    }
-
-    if (light instanceof PointLight) {
-      const w = s.sub(p).unit()
-      const l = light.lightLocation.sub(p).unit()
-
-      const shadowRay = new Line(p.add(l.scale(0.001)), l)
-      const inShadow = n.dot(w) > 0 &&
-        spheres.filter((s) => s != object).some((s) => !!s.intersects(shadowRay))
-
-      if (inShadow) {
-        return new Vector(0, 0, 0)
+    switch (light.type) {
+      case 'ambient': {
+        return cd.scale(kd).mul(cl.scale(ls))
       }
+      case 'directional': {
+        const l = light.location.sub(p).unit()
+        return cd.scale(kd).scale(1 / 3.14).scale(Math.max(0, n.dot(l))).mul(cl.scale(ls))
+      }
+      case 'point': {
+        const w = s.sub(p).unit()
+        const l = light.location.sub(p).unit()
 
-      const diffuseAmount = Math.max(0, n.dot(l))
-      const diffuse = cd.scale(kd).scale(1 / 3.14)
-      .scale(diffuseAmount).mul(cl.scale(ls))
+        const shadowRay = new Line(p.add(l.scale(0.001)), l)
+        const inShadow = n.dot(w) > 0 &&
+          spheres.filter((s) => s != object).some((s) => !!s.intersects(shadowRay))
 
-      const ks = object.specularRefection
-      const e = object.shininess
-      const r = n.scale(2 * diffuseAmount).sub(l)
-      const specularAmount = r.dot(w)
-      const specular = cl.scale(ks * Math.pow(specularAmount, e) * diffuseAmount * ls)
-      return diffuse.add(specular)
+        if (inShadow) {
+          return new V3(0, 0, 0)
+        }
+
+        const diffuseAmount = Math.max(0, n.dot(l))
+        const diffuse = cd.scale(kd).scale(1 / 3.14)
+        .scale(diffuseAmount).mul(cl.scale(ls))
+
+        const ks = object.specularRefection
+        const e = object.shininess
+        const r = n.scale(2 * diffuseAmount).sub(l)
+        const specularAmount = r.dot(w)
+        const specular = cl.scale(ks * Math.pow(specularAmount, e) * diffuseAmount * ls)
+        return diffuse.add(specular)
+      }
     }
   }
 
@@ -313,7 +288,7 @@ const main = () => {
       const ray = new Line(eye, d)
       const color = trace(ray, 0) || background
       const toRGB = (n: number) => Math.max(0, Math.round(Math.min(255, n * 255)))
-      canvas.addPixel(i, j, new Vector(toRGB(color.x), toRGB(color.y), toRGB(color.z)))
+      canvas.addPixel(i, j, new V3(toRGB(color.x), toRGB(color.y), toRGB(color.z)))
     }
   }
 
