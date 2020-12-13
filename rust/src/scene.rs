75a7f95ba9afa8320d3b5be281a8b092dff29987
Boshen <boshenc@gmail.com>
2020-12-14 01:11:42 +0800

    feat(rust): add antialias

--- rust/src/scene.rs
+++ rust/src/scene.rs
@@ -1,6 +1,7 @@
 use image::{RgbImage, Rgb};
 use nalgebra::{Vector3};
 use std::ops::Add;
+use std::ops::Mul;
 
 use crate::model::{Model, Hittable};
 use crate::light::{Light};
@@ -21,17 +22,38 @@ impl Scene {
             for j in 0..self.height {
                 let x = (i as f64)  - (self.width as f64) / 2.0;
                 let y = (j as f64) - (self.height as f64) / 2.0;
-                let d = Vector3::new(x, y, self.focal_length as f64);
-                let ray = Ray {start: self.camera, direction: d};
-                let color = self.trace(&ray);
+                let color = self.antialias(x, y);
                 let rgb = Rgb([
-                    (color.x * 255.0) as u8,
-                    (color.y * 255.0) as u8,
-                    (color.z * 255.0) as u8
+                    self.to_rgb(color.x),
+                    self.to_rgb(color.y),
+                    self.to_rgb(color.z),
                 ]);
                 image.put_pixel(i, j, rgb);
             }
         }
+    }
+
+    fn to_rgb(&self, x: f64) -> u8 {
+        (x * 255.0).min(255.0).round().max(0.0) as u8
+    }
+
+    fn antialias(&self, x: f64, y: f64) -> Vector3<f64> {
+        let n = 5; // sample points
+        let mut color = Vector3::new(0.0, 0.0, 0.0);
+        for i in 0..n {
+            for j in 0..n {
+                let dx = (i as f64 + 0.5) / n as f64;
+                let dy = (j as f64 + 0.5) / n as f64;
+                color = color.add(self.get_color(x + dx, y + dy))
+            }
+        }
+        return color.mul(1.0 / (n as f64 * n as f64))
+    }
+
+    fn get_color(&self, x: f64, y: f64) -> Vector3<f64> {
+        let d = Vector3::new(x, y, self.focal_length as f64);
+        let ray = Ray {start: self.camera, direction: d};
+        return self.trace(&ray)
     }
 
     fn trace(&self, ray: &Ray) -> Vector3<f64> {
