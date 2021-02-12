db7141b1c2dd4fa9e38e3444668b70a8cc3b7d64
Boshen <boshenc@gmail.com>
2021-02-12 23:41:48 +0800

    feat(go): add image

--- go/raytracing.go
+++ go/raytracing.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+    "image"
+    "image/color"
+    "image/png"
+    "os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    width := 500
+    height := 500
+
+    upLeft := image.Point{0, 0}
+    lowRight := image.Point{width, height}
+    img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+    cyan := color.RGBA{100, 200, 200, 0xff}
+
+    for x := 0; x < width; x++ {
+        for y := 0; y < height; y++ {
+            img.Set(x, y, cyan)
+        }
+    }
+
+    // Encode as PNG.
+    f, _ := os.Create("ouput.png")
+    png.Encode(f, img)
 }
